41e68747c02bda2c425189ed299bc8eb3b2dc15d
Edgar Herrador <eherrador@outlook.com>
2015-02-02 13:11:23 -0600

    Se detecta la palabra exacta con Regex.Replace

--- MapeoINEGI/Program.cs
+++ MapeoINEGI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace MapeoINEGI
 {
@@ -8,19 +9,21 @@ namespace MapeoINEGI
 	{
 		public static void Main (string[] args)
 		{
+			int reemplazos = 0;
+
 			string metadatos;
 			string geojson;
 			StringBuilder sb = new StringBuilder();
+			string patron;
 
 			string geojsonPath = @"/Users/eherrador/Desktop/GFK/GeoJSON/Originales/";
-			string geojsonModiPath = @"/Users/eherrador/Desktop/GFK/GeoJSON/Modificados/";
-			string geojsonReportePath = @"/Users/eherrador/Desktop/GFK/GeoJSON/Modificados/Reportes/";
+			string geojsonModiPath = @"/Users/eherrador/Desktop/GFK/GeoJSON/Modificados V2/";
+			string geojsonReportePath = @"/Users/eherrador/Desktop/GFK/GeoJSON/Modificados V2/Reportes/";
 
 			//string geojsonFileName = @"AGSUrbAgeb.json";
 			//string geojsonFileName = @"BCUrbAgeb.json";
 			//string geojsonFileName = @"DFUrbAgeb.json";
 			//string geojsonFileName = @"GTOUrbAgeb.json";
-			//string geojsonFileName = @"HGOUrbAgeb.json";
 			//string geojsonFileName = @"JLUrbAgeb.json";
 			//string geojsonFileName = @"MEXUrbAgeb.json";
 			//string geojsonFileName = @"MORUrbAgeb.json";
@@ -30,7 +33,7 @@ namespace MapeoINEGI
 			//string geojsonFileName = @"SLPUrbAgeb.json";
 			string geojsonFileName = @"SONUrbAgeb.json";
 
-			string metadatosFile = @"/Users/eherrador/Desktop/GFK/GeoJSON/Metadatos/Descripciones_Abreviado.txt";
+			string metadatosFile = @"/Users/eherrador/Desktop/GFK/GeoJSON/Metadatos/TodasDescripciones.txt";
 
 			try {
 				using (StreamReader streamGeoJSON = new StreamReader(geojsonPath + geojsonFileName))
@@ -48,7 +51,11 @@ namespace MapeoINEGI
 							if (geojson.Contains(datos[0])) {
 								Console.WriteLine ("Sustituyendo: " + datos[0] + " -> " + datos[1]);
 								sb.AppendLine("Sustituyendo: " + datos[0] + " -> " + datos[1]);
-								geojson = geojson.Replace(datos[0], datos[1]);
+								//geojson = geojson.Replace(datos[0], datos[1]);
+								patron = "\\b" + datos[0] + "\\b";
+								geojson = Regex.Replace(geojson, patron,  datos[1]);
+								reemplazos++;
+
 							}
 							else {
 								Console.WriteLine("El geojson no contiene a " + datos[0]);
@@ -68,12 +75,15 @@ namespace MapeoINEGI
 				using (StreamWriter outfile = new StreamWriter(geojsonReportePath + geojsonFileName + ".txt"))
 				{
 					outfile.Write(sb.ToString());
+					outfile.WriteLine();
+					outfile.Write("Se realizaron " + reemplazos + " reemplazos");
 				}
 
 				Console.WriteLine("Proceso terminado...");
 				Console.ReadLine();
 			}
 			catch (Exception e) {
+
 				Console.WriteLine (e.Message);
 			}
 		}
